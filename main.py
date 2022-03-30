f717ed66f2c601ce505e80ec51e3243785c7d6d1
kafkas deda <kafkas@gmail.com>
2022-03-30 17:32:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ from clarifai_grpc.grpc.api.status import status_pb2, status_code_pb2
 import mysql.connector
 from mysql.connector import Error
 
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 ## Construct the communications channel and the object stub to call requests on.
 # Note: You can also use a secure (encrypted) ClarifaiChannel.get_grpc_channel() however
 # it is currently not possible to use it with the latest gRPC version
