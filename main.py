38bc6d7a07d12b16240f08b10ae59c1e9d6342b2
kafkas deda <kafkas@gmail.com>
2022-11-02 14:53:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ hide_streamlit_style = """
 {
    visibility: hidden; 
 }
+.viewerBadge_container__1QSob
+{
+   visibility: hidden; 
+}
+
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
